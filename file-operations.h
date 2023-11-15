b5bba77179996a7dca4fee8bca005ef9ea678334
gabriel <gabrieldrt007@gmail.com>
2023-11-14 23:22:42 -0300

    Document file-operations header functions.

--- file-operations.h
+++ file-operations.h
@@ -20,12 +20,24 @@ void openOutputFile(void);
 */
 void closeOutputFile(void);
 
+/** @brief Checks if there is a settings file and creates it if none are found.
+ *  @return Void.
+*/
 void checkSettingsFile(void);
 
+/** @brief Creates a standard settings file based on the original Enigma rotors.
+ *  @return Void.
+*/
 void createDefaultSettingsFile(void);
 
+/** @brief Gets the information stored in the settings file of all three rotors.
+ *  @return A string vector with the lines of the settings of each one of the three rotors.
+*/
 char** getCurrentRotorsInfo(void);
 
+/** @brief Gets the information in the settings of the specified rotor.
+ *  @return A string with the line from the settings with the info of the rotor.
+*/
 char* getCurrentRotorInfo(int rotorNumber);
 
 /** @brief Starts wrigin a new message to the output file.
